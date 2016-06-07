fc12393f3a494d2f233824c0f7879b68310a989f
Graham Taylor <Graham@Grahams-MacBook-Pro.local>
2016-06-07 18:16:22 -0500

    task 5

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 document.querySelector("#thanks button").addEventListener('click',function(){
   // TASK #1
+  // console.log("test")
+  alert("youch! don't push me so hard!")
 })
 
 document.querySelector("#double button").addEventListener('click',function(){
@@ -13,14 +15,34 @@ document.querySelector("#double button").addEventListener('click',function(){
 
 document.querySelector("#color-circle button").addEventListener('click',function(){
   // TASK #3
+  var testCirc = document.querySelector("#circle-bw")
+  console.log(testCirc)
+  // console.log(testCirc.style.backgroundColor)
+  if (testCirc.style.backgroundColor === "white"){
+  	testCirc.style.backgroundColor = "black"
+	} else {
+		testCirc.style.backgroundColor = "white"
+	}
+
 })
 
 document.querySelector("#blow-up button").addEventListener('click',function(){
   // TASK #4}
+  var redCir = document.querySelector("#circle-red")
+  console.log(redCir)
 })
 
 document.querySelector("#remove button").addEventListener('click',function(){
   // TASK #5
+	var ulNode = document.querySelectorAll("#userList li")
+	console.log(ulNode)
+  for (var i = 0; i < ulNode.length; i ++) {
+    var liNode = ulNode[i]
+    console.log(liNode)
+    if (liNode.classList.contains('inactive')) {
+       ulNode.removeChild(liNode)
+    } 
+  }
 })
 
 
